dde54ca1acf8362e9f25923c6cd33a9b4fcc8dd4
Nikola Stanojkovski <65570806+nikolaStanojkovski@users.noreply.github.com>
2022-01-27 20:48:49 +0100

    [no-issue] Main theme design & full authentication mechanism in android application.
    Full authentication mechanism implementation in the android version of the system including Custom email/password authentication, as well as Facebook & Google OAuth.

---
+++ Implementation/android/app/src/main/java/com/musicdistribution/albumdistribution/ui/auth/AuthActivity.kt
@@ -0,0 +1,33 @@
+package com.musicdistribution.albumdistribution.ui.auth
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
+import com.musicdistribution.albumdistribution.data.firebase.auth.FirebaseAuthDB
+import com.musicdistribution.albumdistribution.databinding.ActivityAuthBinding
+import com.musicdistribution.albumdistribution.ui.home.HomeActivity
+
+class AuthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (FirebaseAuthDB.checkLogin()) {
+            FirebaseAuthDB.firebaseAuth.signOut()
+//            navigateOut()
+        }
+
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun navigateOut() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
